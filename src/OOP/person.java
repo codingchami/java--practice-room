c87300b2f47df88ed9a9fb806007aa782291c249
codingchami <cschamithsandeepa@gmail.com>
2024-09-16 23:26:31 +0530

    we use the getName() and setName() methods to access and update the variable

---
+++ src/OOP/person.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class person {
+    public static void main(String[] args) {
+        Encapsulation myObj = new Encapsulation();
+        myObj.setName("John"); // Set the value of the name variable to "John"
+        System.out.println(myObj.getName());
+    }
+
+
+}
