51fe8a513ca41b7d515b46b00ff58d640fc6b154
codingchami <cschamithsandeepa@gmail.com>
2024-09-18 22:44:51 +0530

    Using the Scanner class to get user input:

--- src/OOP/person.java
+++ src/OOP/person.java
@@ -1,11 +1,17 @@
 package OOP;
 
+import java.util.Scanner;
+
 public class person {
     public static void main(String[] args) {
         Encapsulation myObj = new Encapsulation();
         myObj.setName("John"); // Set the value of the name variable to "John"
         System.out.println(myObj.getName());
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Username:");
+        String userName = scanner.nextLine();
+        System.out.println("Username is: "+userName);
     }
-
-
 }
