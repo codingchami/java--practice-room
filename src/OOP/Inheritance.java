2d420bf4e4e38f87ea7d69f825036503cde28ac5
codingchami <cschamithsandeepa@gmail.com>
2024-09-22 23:54:03 +0530

    Car class (subclass) inherits the attributes and methods from the Vehicle class (superclass)

--- src/OOP/Inheritance.java
+++ src/OOP/Inheritance.java
@@ -1,4 +1,19 @@
 package OOP;
 
-public class Inheritance {
+class Vehicle {
+    protected String brand = "BMW";        // Vehicle attribute
+    public void honk() {                    // Vehicle method
+        System.out.println("Tuut, tuut!");
+    }
+}
+public class Inheritance extends Vehicle{
+    private String modelName = "Mustang";
+
+    public static void main(String[] args) {
+        Inheritance inheritance = new Inheritance();
+
+        inheritance.honk();
+
+        System.out.println(inheritance.brand+" "+inheritance.modelName);
+    }
 }
