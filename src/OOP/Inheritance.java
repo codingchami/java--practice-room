6fc984d8b8d456a9e840c94214275e8144ad68d5
codingchami <cschamithsandeepa@gmail.com>
2024-09-22 23:49:45 +0530

    created java class for inheritance

--- src/OOP/Inheritance.java
+++ src/OOP/Inheritance.java
@@ -1,22 +1,4 @@
 package OOP;
-class Vehicle {
-    protected String brand = "BMW";        // Vehicle attribute
-    public void honk() {                    // Vehicle method
-        System.out.println("Tuut, tuut!");
-    }
-}
 
-class Car extends Vehicle {
-    private String modelName = "Mustang";
-    public static void main(String[] args) {
-
-        // Create a myCar object
-        Car myCar = new Car();
-
-        // Call the honk() method (from the Vehicle class) on the myCar object
-        myCar.honk();
-
-        // Display the value of the brand attribute (from the Vehicle class) and the value of the modelName from the Car class
-        System.out.println(myCar.brand + " " + myCar.modelName);
-    }
+public class Inheritance {
 }
