32538bf716c989b638919750c653e3f5356f4273
codingchami <cschamithsandeepa@gmail.com>
2024-09-24 23:34:40 +0530

    cannot inherit from final Vehicle class Inheritance extends Vehicle {

--- src/OOP/Inheritance.java
+++ src/OOP/Inheritance.java
@@ -1,22 +1,22 @@
 package OOP;
 
-class Vehicle {
-    protected String brand = "BMW";        // Vehicle attribute
-    public void honk() {                    // Vehicle method
-        System.out.println("Tuut, tuut!");
-    }
-}
-public class Inheritance extends Vehicle{
-    private String modelName = "Mustang";
-
-    public static void main(String[] args) {
-        Inheritance inheritance = new Inheritance();
-
-        inheritance.honk();
-
-        System.out.println(inheritance.brand+" "+inheritance.modelName);
-    }
-}
+//class Vehicle {
+//    protected String brand = "BMW";        // Vehicle attribute
+//    public void honk() {                    // Vehicle method
+//        System.out.println("Tuut, tuut!");
+//    }
+//}
+//public class Inheritance extends Vehicle{
+//    private String modelName = "Mustang";
+//
+//    public static void main(String[] args) {
+//        Inheritance inheritance = new Inheritance();
+//
+//        inheritance.honk();
+//
+//        System.out.println(inheritance.brand+" "+inheritance.modelName);
+//    }
+//}
 
 
 //final class Vehicle {
@@ -26,3 +26,32 @@ public class Inheritance extends Vehicle{
 //class Car extends Vehicle {
 //  ...
 //}
+
+final class Vehicle {
+    protected String brand = "Ford";
+    public void honk() {
+        System.out.println("Tuut, tuut!");
+    }
+}
+
+public class Inheritance extends Vehicle {
+    private String modelName = "Mustang";
+    public static void main(String[] args) {
+        Inheritance myFastCar = new Inheritance();
+        myFastCar.honk();
+        System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
