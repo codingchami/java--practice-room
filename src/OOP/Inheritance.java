0b07fdb38b41416881bbf541198f73d0a7dac529
codingchami <cschamithsandeepa@gmail.com>
2024-09-26 14:41:39 +0530

    Declare three variables and assign them with values

--- src/OOP/Inheritance.java
+++ src/OOP/Inheritance.java
@@ -27,21 +27,21 @@ package OOP;
 //  ...
 //}
 
-final class Vehicle {
-    protected String brand = "Ford";
-    public void honk() {
-        System.out.println("Tuut, tuut!");
-    }
-}
-
-public class Inheritance extends Vehicle {
-    private String modelName = "Mustang";
-    public static void main(String[] args) {
-        Inheritance myFastCar = new Inheritance();
-        myFastCar.honk();
-        System.out.println(myFastCar.brand + " " + myFastCar.modelName);
-    }
-}
+//final class Vehicle {
+//    protected String brand = "Ford";
+//    public void honk() {
+//        System.out.println("Tuut, tuut!");
+//    }
+//}
+//
+//public class Inheritance extends Vehicle {
+//    private String modelName = "Mustang";
+//    public static void main(String[] args) {
+//        Inheritance myFastCar = new Inheritance();
+//        myFastCar.honk();
+//        System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+//    }
+//}
 
 
 
---
+++ src/OOP_Anju/Encapsulation.java
@@ -0,0 +1,16 @@
+package OOP_Anju;
+
+public class Encapsulation {
+    public static void main(String[] args) {
+
+        int length;
+        int width;
+        int height;
+
+        length = 3;
+        width = 5;
+        height = 3;
+
+
+    }
+}
