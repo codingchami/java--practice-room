ecd35d5587aaa5026009768a3588e2c460fee514
codingchami <cschamithsandeepa@gmail.com>
2024-09-26 15:13:52 +0530

    create again class name box and declare previous variables again

--- src/OOP_Anju/Encapsulation.java
+++ src/OOP_Anju/Encapsulation.java
@@ -1,23 +1,31 @@
 package OOP_Anju;
 
+class Box{
+    int width;
+    int height;
+    int length;
+
+
+}
+
 public class Encapsulation {
     public static void main(String[] args) {
 
-        int length;
-        int width;
-        int height;
-
-        length = 3;
-        width = 5;
-        height = 3;
-
-        int volume;
-        volume = length*width*height;
-        System.out.println("Volume of the box is :"+ volume);
-
-        int area;
-        area = 2*length*width+2*length*height+2*width*height;
-        System.out.println("Area of the box is :"+area);
+//        int length;
+//        int width;
+//        int height;
+//
+//        length = 3;
+//        width = 5;
+//        height = 3;
+//
+//        int volume;
+//        volume = length*width*height;
+//        System.out.println("Volume of the box is :"+ volume);
+//
+//        int area;
+//        area = 2*length*width+2*length*height+2*width*height;
+//        System.out.println("Area of the box is :"+area);
 
 
 
