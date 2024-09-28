774e399bd324a96316fbd2dd47cb975c57848e5d
codingchami <cschamithsandeepa@gmail.com>
2024-09-28 12:26:29 +0530

    in crrate the getVolume method with int returntype method

--- src/OOP_Anju/Encapsulation.java
+++ src/OOP_Anju/Encapsulation.java
@@ -5,6 +5,29 @@ class Box{
     int height;
     int length;
 
+    public void setSize(int l,int w,int h){
+        length = l;
+        width = w;
+        height = h;
+    }
+
+//    public void printVolume(){
+//        int volume;
+//        volume = length*width*height;
+//        System.out.println("Volume is : "+volume);
+//    }
+//    public void printArea(){
+//        int area;
+//        area = 2*length*width+2*length*height+2*width*height;
+//        System.out.println("Area of the box is : "+area);
+//
+//    }
+
+    public int getVolume(){
+        int volume = length*width*height;
+        return volume;
+    }
+
 
 }
 
@@ -13,8 +36,8 @@ public class Encapsulation {
 
         Box b1 = new Box();
         b1.setSize(5,3,2);
-        b1.printVolume();
-        b1.printArea();
+//        b1.printVolume();
+//        b1.printArea();
 
 //        int length;
 //        int width;
