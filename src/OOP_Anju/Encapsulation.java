216a0390fa7efdfaa9e9af7ae3475ec1e7511f9a
codingchami <cschamithsandeepa@gmail.com>
2024-09-28 15:03:40 +0530

    Construcor has the return type..(Constructor returns the object location)

--- src/OOP_Anju/Encapsulation.java
+++ src/OOP_Anju/Encapsulation.java
@@ -5,7 +5,17 @@ class Box{
     int height;
     int length;
 
-    public void setSize(int length,int width,int height){
+    public Box(){
+
+    }
+
+    public Box(int width, int height, int length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public void setSize(int length, int width, int height){
         length = length;
         width = width;
         height = height;
@@ -34,10 +44,10 @@ class Box{
 public class Encapsulation {
     public static void main(String[] args) {
 
-        Box b1 = new Box();
-        b1.setSize(5,3,2);
-
-        System.out.println("Volume of the box is "+b1.getVolume());
+//        Box b1 = new Box();
+//        b1.setSize(5,3,2);
+//
+//        System.out.println("Volume of the box is "+b1.getVolume());
 //        b1.printVolume();
 //        b1.printArea();
 
@@ -56,6 +66,10 @@ public class Encapsulation {
 //        int area;
 //        area = 2*length*width+2*length*height+2*width*height;
 //        System.out.println("Area of the box is :"+area);
+
+
+        Box bs = new Box();
+        System.out.println(bs);
 
 
 
