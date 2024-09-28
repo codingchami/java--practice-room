59840d208bdda418ce2354b2109037e5785339dc
codingchami <cschamithsandeepa@gmail.com>
2024-09-28 12:28:03 +0530

    return volume catch by sout in the main method

--- src/OOP_Anju/Encapsulation.java
+++ src/OOP_Anju/Encapsulation.java
@@ -36,6 +36,8 @@ public class Encapsulation {
 
         Box b1 = new Box();
         b1.setSize(5,3,2);
+
+        System.out.println("Volume of the box is "+b1.getVolume());
 //        b1.printVolume();
 //        b1.printArea();
 
