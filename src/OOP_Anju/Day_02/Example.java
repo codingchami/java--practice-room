129c87c930633e7c4c3b9982ef20910eaf43a805
codingchami <cschamithsandeepa@gmail.com>
2024-09-30 10:37:13 +0530

    d1 and d2 reference variables are representing the same object,so d1==d2 gives the true..

--- src/OOP_Anju/Day_02/Example.java
+++ src/OOP_Anju/Day_02/Example.java
@@ -1,24 +1,49 @@
 package OOP_Anju.Day_02;
 
-class Value{
-    int a;
-    Value(int a){
-        this.a = a;
+//class Value{
+//    int a;
+//    Value(int a){
+//        this.a = a;
+//    }
+//}
+//
+//class ChangeValue{
+//    public static void increment(Value v){
+//        v.a++;
+//    }
+//}
+//
+//public class Example {
+//    public static void main(String[] args) {
+//        Value v1 = new Value(100);
+//        System.out.println(v1.a);
+//
+//        ChangeValue.increment(v1); //method call by reference
+//        System.out.println(v1.a);
+//    }
+//}
+
+
+class Dog{
+    String name;
+    int age;
+
+    Dog(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public Dog getCopy(){
+        return this;  //its returns the object when currently running one
     }
 }
 
-class ChangeValue{
-    public static void increment(Value v){
-        v.a++;
-    }
-}
 
-public class Example {
+
+public class Example{
     public static void main(String[] args) {
-        Value v1 = new Value(100);
-        System.out.println(v1.a);
-
-        ChangeValue.increment(v1); //method call by reference
-        System.out.println(v1.a);
+        Dog d1 = new Dog("Tommy",2);
+        Dog d2 = d1.getCopy(); //create the new reference varibale and store the object what returns by getCopy method
+        System.out.println(d1==d2);
     }
 }
