a46e426a9addf6a488ee52b69fb793307496b826
codingchami <cschamithsandeepa@gmail.com>
2024-10-02 19:18:18 +0530

    Example for pre-defined methods

--- src/OOP_Anju/Day_02/Example.java
+++ src/OOP_Anju/Day_02/Example.java
@@ -141,50 +141,64 @@ package OOP_Anju.Day_02;
 
 //********************************************************************************************************
 
-class Customer{
-    int id;
-    String name;
+//class Customer{
+//    int id;
+//    String name;
+//
+//    Customer(){}     //same class, same name, different parametr signature (method overloading)
+//
+//    Customer(int id,String name){
+//        this.id = id;
+//        this.name = name;
+//    }
+//
+//    public void printCustomer(){
+//        System.out.println(id+"-"+name);
+//    }
+//    public void setCustomer(int id,String name){
+//        this.id = id;
+//        this.name = name;
+//    }
+//    public void setCustomer(Customer c1){
+//        this.id = c1.id;
+//        this.name = c1.name;
+//    }
+//}
+//
+//public class Example{
+//    public static void main(String[] args) {
+////        Customer c1 = new Customer();
+//
+////        new Customer().id = 123455678;
+////        new Customer().name = "Kamal"; //Annonymous object(it takes the minimum space of the ram)& after the related work JVM it removes.
+//
+//        Customer c1 = new Customer(10,"Nimal");
+//        Customer c2 = new Customer();
+////         c2.setCustomer(15,"Chamith");
+//
+//         c1.printCustomer(); //10-Nimal
+//         c2.printCustomer(); //0-null
+//
+//        c2.setCustomer(c1);
+//        c1.printCustomer();//10-Nimal
+//        c2.printCustomer();//
+//
+//
+//    }
+//}
 
-    Customer(){}     //same class, same name, different parametr signature (method overloading)
 
-    Customer(int id,String name){
-        this.id = id;
-        this.name = name;
-    }
+//********************************************************************************************************
 
-    public void printCustomer(){
-        System.out.println(id+"-"+name);
-    }
-    public void setCustomer(int id,String name){
-        this.id = id;
-        this.name = name;
-    }
-    public void setCustomer(Customer c1){
-        this.id = c1.id;
-        this.name = c1.name;
-    }
-}
+import javax.swing.*;
 
 public class Example{
     public static void main(String[] args) {
-//        Customer c1 = new Customer();
-
-//        new Customer().id = 123455678;
-//        new Customer().name = "Kamal"; //Annonymous object(it takes the minimum space of the ram)& after the related work JVM it removes.
-
-        Customer c1 = new Customer(10,"Nimal");
-        Customer c2 = new Customer();
-         c2.setCustomer(15,"Chamith");
-
-         c1.printCustomer(); //10-Nimal
-         c2.printCustomer(); //15-Chamith
-
-
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setTitle("Java");
+        frame.setVisible(true);
     }
 }
-
-
-
-
 
 
