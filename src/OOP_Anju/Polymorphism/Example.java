d5fef4d801f86b19e0a0db444ed34a37482d3cab
codingchami <cschamithsandeepa@gmail.com>
2024-10-07 16:04:00 +0530

    check how to works super key word

--- src/OOP_Anju/Polymorphism/Example.java
+++ src/OOP_Anju/Polymorphism/Example.java
@@ -1,19 +1,47 @@
 package OOP_Anju.Polymorphism;
 
-class Animal{
+/*class Animal{
     public void sound(){
         System.out.println("Animal Sound");
     }
 }
 class Cat extends Animal{
     public void sound(){
-        System.out.println("Cat sound");
+        System.out.println("Cat sound");//now its strat the method overriding,different classes,same name,same parameters
     }
 }
 
+//method overriding --different classes,same name,same parameters
+//method overloading --same class,same name,different parameters
+
 public class Example {
     public static void main(String[] args) {
-        Cat c = new Cat(); //here we can access sub class instance through super class things
+        Cat c = new Cat(); //here we can access subclass instance through super class things
         c.sound(); //Animal Sound
+    }
+}*/
+
+class A{
+    public void print(){
+        System.out.println("Print A");
+    }
+}
+class B extends A{
+    public void print(){
+        System.out.println("Print B");
+    }
+//    public void print(int i){}
+//}
+    public void callPrint(){
+        print(); //from subclass
+        super.print(); //from super class
+    }
+}
+
+public class Example{
+    public static void main(String[] args) {
+        B b = new B();
+        b.print(); //method overriding --(it happens on runtime)
+//        b.print(10);//method overloading --(it happens on compile time)
     }
 }
