ecd4cb1322de0e26b62a42d3732671395b336c4c
codingchami <cschamithsandeepa@gmail.com>
2024-10-07 15:24:30 +0530

    now it face to the method overriding...(Brcause animal and cat are different classes..same name,same parameters)

--- src/OOP_Anju/Polymorphism/Example.java
+++ src/OOP_Anju/Polymorphism/Example.java
@@ -6,8 +6,14 @@ class Animal{
     }
 }
 class Cat extends Animal{
-
+    public void sound(){
+        System.out.println("Cat sound");
+    }
 }
 
 public class Example {
+    public static void main(String[] args) {
+        Cat c = new Cat(); //here we can access sub class instance through super class things
+        c.sound(); //Animal Sound
+    }
 }
