d67ff4c40e9aff3d7039adcce0a601ebfbb3fee5
codingchami <cschamithsandeepa@gmail.com>
2024-10-07 15:15:09 +0530

    create a Animal class(parent class) & Cat class(child class)

--- src/OOP_Anju/Polymorphism/Example.java
+++ src/OOP_Anju/Polymorphism/Example.java
@@ -1,4 +1,13 @@
 package OOP_Anju.Polymorphism;
 
+class Animal{
+    public void sound(){
+        System.out.println("Animal Sound");
+    }
+}
+class Cat extends Animal{
+
+}
+
 public class Example {
 }
